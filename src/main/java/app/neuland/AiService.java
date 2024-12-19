c70a624189e9522c385631a13b30e3f9baf7bff3
Markus Herhoffer <markus@herhoffer.net>
2024-12-19 19:29:38 +0100

    🐛 (year): Improve year bug even more

--- src/main/java/app/neuland/AiService.java
+++ src/main/java/app/neuland/AiService.java
@@ -10,7 +10,7 @@ public interface AiService
 {
 	@SystemMessage("You are a webscraper that serializes a week menu of meals into objects by day")
 	@UserMessage("""
-		Scrape the HTML {html} and serialize it into a JSON. Use name_de for the German name of the meal, translate to English for name_en. 
-		The current date is {current_date} and the dates on the menu are always the current week, so set the year to the current year.""")
+		Scrape the HTML {html} and serialize it into a JSON. Use name_de for the German name of the meal, translate to English for name_en.
+		The dates on the menu are always the current week, so set the year to the year of {current_date}.""")
 	Menu scrapeMeals(String html);
 }
